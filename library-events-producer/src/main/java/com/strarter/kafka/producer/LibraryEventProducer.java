6d44943c936d05decea8dd9c50eba390b79ca0e1
DeevS <mygrayroom@gmail.com>
2024-01-24 20:38:49 +0300

    Section 7. Events producer. 1 Approach

---
+++ library-events-producer/src/main/java/com/strarter/kafka/producer/LibraryEventProducer.java
@@ -0,0 +1,55 @@
+package com.strarter.kafka.producer;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.strarter.kafka.domain.LibraryEvent;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.NonFinal;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CompletableFuture;
+
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@RequiredArgsConstructor
+@Slf4j
+@Component
+public class LibraryEventProducer {
+
+    KafkaTemplate<Integer, String> kafkaTemplate;
+    ObjectMapper objectMapper;
+
+    @NonFinal
+    @Value("${kafka.topic.library}")
+    public String topic;
+
+    public CompletableFuture<SendResult<Integer, String>> sendLibraryEvent(LibraryEvent libraryEvent) throws JsonProcessingException {
+        Integer key = libraryEvent.libraryEventId();
+        String value = objectMapper.writeValueAsString(libraryEvent);
+
+        CompletableFuture<SendResult<Integer, String>> completableFuture = kafkaTemplate.send(topic, key, value);
+
+        return completableFuture.whenComplete((sendResult, throwable) -> {
+            if (throwable != null) {
+                handleError(throwable);
+            } else {
+                handleSuccess(key, value, sendResult);
+            }
+        });
+    }
+
+    private void handleSuccess(Integer key, String value, SendResult<Integer, String> sendResult) {
+        log.info("Message sent successfully for the key : {} and the value : {} , partition is {} ",
+                key, value, sendResult.getRecordMetadata().partition());
+    }
+
+    private void handleError(Throwable throwable) {
+        log.error("Error sending the message and the exception is {} ", throwable.getMessage(), throwable);
+    }
+
+}
