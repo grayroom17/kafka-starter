14dd8b25b978b8166702e26cd2ccf32df06b2205
DeevS <mygrayroom@gmail.com>
2024-01-25 19:22:04 +0300

    Section 7. Events producer. 1 Approach. Part 2

--- library-events-producer/src/main/java/com/strarter/kafka/controller/LibraryEventsController.java
+++ library-events-producer/src/main/java/com/strarter/kafka/controller/LibraryEventsController.java
@@ -1,6 +1,11 @@
 package com.strarter.kafka.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.strarter.kafka.domain.LibraryEvent;
+import com.strarter.kafka.producer.LibraryEventProducer;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,15 +14,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@RequiredArgsConstructor
 @Slf4j
 @RestController
 @RequestMapping("/v1")
 public class LibraryEventsController {
 
+    LibraryEventProducer libraryEventProducer;
+
     @PostMapping("/library-event")
-    ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) {
+    ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException {
         log.info("libraryEvent : {} ", libraryEvent);
-        //invoke kafka producer
+        libraryEventProducer.sendLibraryEvent(libraryEvent);
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
 
