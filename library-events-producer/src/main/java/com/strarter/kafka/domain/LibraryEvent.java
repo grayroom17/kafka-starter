162124fa86250cb88e50b4c44ce2177d2d60ccef
DeevS <mygrayroom@gmail.com>
2024-01-30 19:46:41 +0300

    Section 9. SpringBoot Unit tests. Test validation of controller

--- library-events-producer/src/main/java/com/strarter/kafka/domain/LibraryEvent.java
+++ library-events-producer/src/main/java/com/strarter/kafka/domain/LibraryEvent.java
@@ -1,8 +1,13 @@
 package com.strarter.kafka.domain;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+
 public record LibraryEvent(
         Integer libraryEventId,
         LibraryEventType libraryEventType,
+        @Valid
+        @NotNull
         Book book
 ) {
 }
