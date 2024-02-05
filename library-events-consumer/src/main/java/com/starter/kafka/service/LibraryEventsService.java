145bd3056c816d380196cfeeb2984bc5dc6d1e02
DeevS <mygrayroom@gmail.com>
2024-02-05 20:07:11 +0300

    Section 14. Validation on create and update LibraryEvent

--- library-events-consumer/src/main/java/com/starter/kafka/service/LibraryEventsService.java
+++ library-events-consumer/src/main/java/com/starter/kafka/service/LibraryEventsService.java
@@ -11,6 +11,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.stereotype.Service;
 
+import static com.starter.kafka.entity.LibraryEventType.NEW;
+import static com.starter.kafka.entity.LibraryEventType.UPDATE;
+
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @RequiredArgsConstructor
 @Slf4j
@@ -23,7 +26,27 @@ public class LibraryEventsService {
     public void processLibraryEvent(ConsumerRecord<Integer, String> consumerRecord) throws JsonProcessingException {
         LibraryEvent libraryEvent = objectMapper.readValue(consumerRecord.value(), LibraryEvent.class);
         log.info("libraryEvent : {}", libraryEvent);
+        validate(libraryEvent);
         save(libraryEvent);
+    }
+
+    private void validate(LibraryEvent libraryEvent) {
+        if (libraryEvent.getLibraryEventType().equals(NEW)) {
+            if (libraryEvent.getLibraryEventId() != null) {
+                throw new IllegalArgumentException("LibraryEvent id should be null when create new LibraryEvent");
+            }
+            return;
+        }
+        if (libraryEvent.getLibraryEventType().equals(UPDATE)) {
+            if (libraryEvent.getLibraryEventId() == null) {
+                throw new IllegalArgumentException("LibraryEvent id should be not null when update LibraryEvent");
+            }
+            libraryEventsRepository.findById(libraryEvent.getLibraryEventId())
+                    .orElseThrow(() -> new IllegalArgumentException("Not a valid LibraryEvent"));
+            log.info("Validation is successful for the LibraryEvent : {}", libraryEvent);
+            return;
+        }
+        throw new IllegalArgumentException("Invalid LibraryEvent Type");
     }
 
     private void save(LibraryEvent libraryEvent) {
