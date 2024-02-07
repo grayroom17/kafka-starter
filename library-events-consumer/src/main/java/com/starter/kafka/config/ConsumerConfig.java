014e198ef82137086da9055799f311090aaf856f
DeevS <mygrayroom@gmail.com>
2024-02-07 19:43:52 +0300

    Section 16. Error Handling. Retry Listener. Logging Additional information on each retry

--- library-events-consumer/src/main/java/com/starter/kafka/config/ConsumerConfig.java
+++ library-events-consumer/src/main/java/com/starter/kafka/config/ConsumerConfig.java
@@ -1,5 +1,6 @@
 package com.starter.kafka.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,12 +9,18 @@ import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.util.backoff.FixedBackOff;
 
+@Slf4j
 @Configuration
 public class ConsumerConfig {
 
     public DefaultErrorHandler errorHandler() {
         FixedBackOff fixedBackOff = new FixedBackOff(1000L, 2);
-        return new DefaultErrorHandler(fixedBackOff);
+        DefaultErrorHandler errorHandler = new DefaultErrorHandler(fixedBackOff);
+        errorHandler.setRetryListeners((consumerRecord, ex, deliveryAttempt)
+                -> log.info("Failed Record in Retry Listener, Exception : {} , deliveryAttempt : {} ",
+                ex.getMessage(),
+                deliveryAttempt));
+        return errorHandler;
     }
 
     @Bean
