ea11f688ae6afcc265a7542640828bd15b80b5c2
DeevS <mygrayroom@gmail.com>
2024-02-13 20:59:26 +0300

    Section 16. Recovery. ConsumerRecordRecoverer.Retry persisted failed records

--- library-events-consumer/src/main/java/com/starter/kafka/repository/FailureRecordRepository.java
+++ library-events-consumer/src/main/java/com/starter/kafka/repository/FailureRecordRepository.java
@@ -3,7 +3,10 @@ package com.starter.kafka.repository;
 import com.starter.kafka.entity.FailureRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FailureRecordRepository extends JpaRepository<FailureRecord, Integer> {
 
+    List<FailureRecord> findAllByStatus(String retry);
 
 }
