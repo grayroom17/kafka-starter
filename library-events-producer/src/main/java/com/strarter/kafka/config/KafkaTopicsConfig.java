adef0ef08b05395423ca9db796ca6a9e5726e62e
DeevS <mygrayroom@gmail.com>
2024-02-20 20:54:38 +0300

    Section 19. Producer SSL configuration

--- library-events-producer/src/main/java/com/strarter/kafka/config/KafkaTopicsConfig.java
+++ library-events-producer/src/main/java/com/strarter/kafka/config/KafkaTopicsConfig.java
@@ -1,24 +1,24 @@
-package com.strarter.kafka.config;
-
-import org.apache.kafka.clients.admin.NewTopic;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.config.TopicBuilder;
-
-@Configuration
-public class KafkaTopicsConfig {
-
-    @Value("${spring.kafka.template.default-topic}")
-    String eventName;
-
-    @Bean
-    public NewTopic libraryEvents() {
-        return TopicBuilder
-                .name(eventName)
-                .partitions(3)
-                .replicas(3)
-                .build();
-    }
-
-}
+//package com.strarter.kafka.config;
+//
+//import org.apache.kafka.clients.admin.NewTopic;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.kafka.config.TopicBuilder;
+//
+//@Configuration
+//public class KafkaTopicsConfig {
+//
+//    @Value("${spring.kafka.template.default-topic}")
+//    String eventName;
+//
+//    @Bean
+//    public NewTopic libraryEvents() {
+//        return TopicBuilder
+//                .name(eventName)
+//                .partitions(3)
+//                .replicas(3)
+//                .build();
+//    }
+//
+//}
